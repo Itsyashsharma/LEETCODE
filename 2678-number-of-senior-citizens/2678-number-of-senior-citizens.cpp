a53373e97c8f3bd4c323edac66d8dd67a869224b
Yash Sharma <87236354+Itsyashsharma@users.noreply.github.com>
2023-05-24 12:42:14 +0530

    Time: 8 ms (74.11%), Space: 13.6 MB (6.83%) - LeetHub

--- 2678-number-of-senior-citizens/2678-number-of-senior-citizens.cpp
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.cpp
@@ -3,7 +3,6 @@ public:
     int countSeniors(vector<string>& details) {
         int x = 0;
         for(auto &i: details){
-            // cout<<i[11]<<" "<<i[12]<<endl;
             if(i[11]>'6'){
                 x++;
             }else if(i[11]=='6'&&i[12]>'0')x++;
