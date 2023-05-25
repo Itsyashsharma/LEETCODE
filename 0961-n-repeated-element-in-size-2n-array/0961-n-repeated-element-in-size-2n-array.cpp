1aee0f630d5fc93977b150c27b3034661eabc48a
Yash Sharma <87236354+Itsyashsharma@users.noreply.github.com>
2023-05-26 02:09:41 +0530

    Time: 32 ms (86.77%), Space: 24.8 MB (59.37%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
-    int repeatedNTimes(vector<int>& nums) {
-        int n = nums.size();
-        map<int,int> mp;
-        for(int i=0; i<n; i++) {
-            mp[nums[i]]++;
+    int repeatedNTimes(vector<int>& nums)
+    {
+        map<int, int> count;
+        for(int i=0; i<nums.size()/2+2; i++)
+        {
+            if(count[nums[i]] == 1) return nums[i];
+            count[nums[i]]++;
         }
-        for(auto it: mp) {
-            if(it.second >= n/2)
-                return it.first;
-        }
-        return -1;
+        return 0;
     }
 };
