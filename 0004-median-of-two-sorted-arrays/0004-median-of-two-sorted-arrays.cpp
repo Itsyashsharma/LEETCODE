8b8c1cf598faefa1cc7cedebf7f18f2d61d4320d
Yash Sharma <87236354+Itsyashsharma@users.noreply.github.com>
2023-09-19 22:52:38 +0530

    Time: 33 ms (31.43%), Space: 89.6 MB (45.77%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,33 +1,45 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        
-        // Create a single sorted by merging two sorted arrays
-        int n1=nums1.size();
-        int n2=nums2.size();
-        int i=0;
-        int j=0;
-        int lastindex=-1;
-             
-        // Initialize a new array
-           vector<int>v(n1+n2,0);
-        
-        while(i<n1&&j<n2)
+        if (nums2.size() < nums1.size())
         {
-            if(nums1[i]<=nums2[j])
-                v[++lastindex]=nums1[i++];
-            else
-                v[++lastindex]=nums2[j++];
+            return findMedianSortedArrays(nums2, nums1);
         }
-        
-        while(i<n1)
-            v[++lastindex]=nums1[i++];
-        while(j<n2)
-            v[++lastindex]=nums2[j++];
-        
-    // Return the result
-        int n=n1+n2;
-        return n%2?v[n/2]:(v[n/2]+v[n/2-1])/2.0;
-        
+        int total = nums1.size() + nums2.size() + 1;
+        int left = (total) / 2;
+        int l = 0 , h = nums1.size();
+
+        while(l <= h)
+        {
+            int m = l + (h - l) / 2;
+            int n1 = m , n2 = left - m;
+            int l1 = n1 == 0 ? INT_MIN : nums1[n1-1];
+            int l2 = n2 == 0 ? INT_MIN : nums2[n2-1];
+            int r1 = n1 == nums1.size() ? INT_MAX : nums1[n1];
+            int r2 = n2 == nums2.size() ? INT_MAX : nums2[n2];
+
+            if(l1 <= r2 && l2 <= r1)
+            {
+                double median;
+                if((nums1.size() + nums2.size()) % 2 == 0)
+                {
+                    median = (max(l1,l2) + min(r1,r2))/2.0;
+                }
+                else
+                {
+                    median = max(l1,l2);
+                }
+                return median;
+            }
+            else if(l1 > r2)
+            {
+                h = m - 1;
+            }
+            else
+            {
+                l = m + 1;
+            }
+        }
+        return -1;
     }
 };
